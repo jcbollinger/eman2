2b5066d12384ce2bba60f39092318f021029c33e
shadow_walker <shadowwalkersb@gmail.com>
2019-11-17 22:44:35 -0600

    messages [skip travis] [skip appveyor]

--- cmake/functions.cmake
+++ cmake/functions.cmake
@@ -1,10 +1,1 @@
 include(CMakePrintHelpers)
-
-
-function(is_target_exist trgt)
-	if(TARGET ${trgt})
-		message("Target ${trgt} exists.")
-	else()
-		message("Target ${trgt} does NOT exist.")
-	endif()
-endfunction()
