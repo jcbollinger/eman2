8363be3a312d46933ef0a5d6d2fac57acbb7c831
shadow_walker <shadowwalkersb@gmail.com>
2019-11-17 23:37:07 -0600

    SP_DIR [skip travis] [skip appveyor]

--- cmake/FindPython.cmake
+++ cmake/FindPython.cmake
@@ -10,22 +10,6 @@ message("PYTHON_INCLUDE_DIRS: ${PYTHON_INCLUDE_DIRS}")
 message("PYTHON_INCLUDE_PATH: ${PYTHON_INCLUDE_PATH}")
 message("PYTHON_INCLUDE_DIR:  ${PYTHON_INCLUDE_DIR}")
 
-# Set SP_DIR
-if("$ENV{CONDA_BUILD_STATE}" STREQUAL "BUILD" )
-	set(SP_DIR $ENV{SP_DIR})
-else()
-	if(NOT WIN32)
-		set(py_sp_dir_command "import site; print(site.getsitepackages()[0])")
-	else()
-		set(py_sp_dir_command "import site; print(site.getsitepackages()[1])")
-	endif()
-	execute_process(COMMAND ${PYTHON_EXECUTABLE} -c "${py_sp_dir_command}"
-			OUTPUT_VARIABLE SP_DIR
-			OUTPUT_STRIP_TRAILING_WHITESPACE
-			)
-	message("Python site-packages: ${SP_DIR}")
-endif()
-
 execute_process(COMMAND ${PYTHON_EXECUTABLE} -c "import sysconfig; print(sysconfig.get_config_var('Py_ENABLE_SHARED'))"
 				OUTPUT_VARIABLE PYTHON_LIB_SHARED
 				OUTPUT_STRIP_TRAILING_WHITESPACE
@@ -36,7 +20,7 @@ cmake_print_variables(CMAKE_CXX_COMPILER_ID)
 
 include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(Python
-		REQUIRED_VARS PYTHON_EXECUTABLE PYTHON_LIBRARIES PYTHON_INCLUDE_DIR SP_DIR
+		REQUIRED_VARS PYTHON_EXECUTABLE PYTHON_LIBRARIES
 		)
 
 # The python interpreter can be linked against shared libpython.so or static libpython.a.
