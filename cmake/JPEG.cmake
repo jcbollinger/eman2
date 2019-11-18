f4b61275eb0f3854a4ad902f70c84e330a5dee3d
shadow_walker <shadowwalkersb@gmail.com>
2019-11-17 21:34:17 -0600

    cleanup [skip travis] [skip appveyor]

--- cmake/JPEG.cmake
+++
@@ -1,9 +0,0 @@
-find_package(JPEG REQUIRED)
-
-cmake_print_variables(JPEG_LIBRARIES)
-
-if(JPEG_FOUND)
-	set_target_properties(JPEG::JPEG PROPERTIES
-						  INTERFACE_COMPILE_DEFINITIONS USE_JPEG
-						  )
-endif()
--- cmake/TIFF.cmake
+++
@@ -1,9 +0,0 @@
-find_package(TIFF REQUIRED)
-
-cmake_print_variables(TIFF_LIBRARIES)
-
-if(TIFF_FOUND)
-	set_target_properties(TIFF::TIFF PROPERTIES
-			INTERFACE_COMPILE_DEFINITIONS USE_TIFF
-			)
-endif()
