1408e8c540519d131839364dfe2d64831d8fd803
shadow_walker <shadowwalkersb@gmail.com>
2019-11-17 23:02:51 -0600

    messages [skip travis] [skip appveyor]

--- cmake/OpenGL.cmake
+++ cmake/OpenGL.cmake
@@ -2,17 +2,6 @@ set(OpenGL_GL_PREFERENCE GLVND)
 
 find_package(OpenGL REQUIRED COMPONENTS OpenGL)
 
-cmake_print_variables(OPENGL_INCLUDE_DIR)
-cmake_print_variables(OPENGL_LIBRARIES)
-cmake_print_variables(OPENGL_glu_LIBRARY)
-cmake_print_variables(OPENGL_opengl_LIBRARY)
-cmake_print_variables(OPENGL_gl_LIBRARY)
-cmake_print_variables(OPENGL_FOUND)
-cmake_print_variables(OPENGL_GLU_FOUND)
-cmake_print_variables(OpenGL_OpenGL_FOUND)
-cmake_print_variables(OPENGL_opengl_LIBRARY)
-cmake_print_variables(OPENGL_gl_LIBRARY)
-
 if(OpenGL_FOUND AND NOT TARGET OpenGL AND NOT TARGET EMAN::OpenGL)
 	add_library(OpenGL INTERFACE)
 	add_library(EMAN::OpenGL ALIAS OpenGL)
