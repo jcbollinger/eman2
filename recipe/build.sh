2ead4c468a2633ec3b18e92252d3802ea061d898
shadow_walker <shadowwalkersb@gmail.com>
2019-11-29 18:09:44 -0600

    stash

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,8 +2,8 @@
 
 set -xe
 
-$BUILD_PREFIX/bin/x86_64-conda_cos6-linux-gnu-c++  \
--isystem $PREFIX/include/python2.7 \
--isystem $BUILD_PREFIX/x86_64-conda_cos6-linux-gnu/sysroot/usr/include  \
+${CXX}  -c $SRC_DIR/hello-boost.cpp \
 -I$PREFIX/include \
--c $SRC_DIR/hello-boost.cpp
+-isystem $PREFIX/include/python2.7 \
+-isystem $BUILD_PREFIX/x86_64-conda_cos6-linux-gnu/sysroot/usr/include
+#${CXXFLAGS} \
